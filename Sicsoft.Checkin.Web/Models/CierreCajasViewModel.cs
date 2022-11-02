f52e647c74f6e04584a2f3b7eb78fea0b6ed60b9
srodriguez09 <110418699+srodriguez09@users.noreply.github.com>
2022-11-02 16:39:13 -0600

    Cambios

--- Sicsoft.Checkin.Web/Models/CierreCajasViewModel.cs
+++ Sicsoft.Checkin.Web/Models/CierreCajasViewModel.cs
@@ -6,59 +6,59 @@ namespace NOVAAPP.Models
 {
     public class CierreCajasViewModel
     {
-       
+
         public int idUsuario { get; set; }
 
 
         public int idCaja { get; set; }
 
-    
+
         public DateTime FechaCaja { get; set; }
 
         public DateTime FecUltAct { get; set; }
 
-      
+
         public string IP { get; set; }
 
-      
+
         public decimal EfectivoColones { get; set; }
 
-     
-        public decimal ChequesColones { get; set; }  
-         
+
+        public decimal ChequesColones { get; set; }
+
         public decimal TarjetasColones { get; set; }
 
-        
+
         public decimal OtrosMediosColones { get; set; }
 
-       
+
         public decimal TotalVendidoColones { get; set; }
 
-       
+
         public decimal TotalRegistradoColones { get; set; }
 
-       
+
         public decimal TotalAperturaColones { get; set; }
 
-       
+
         public decimal EfectivoFC { get; set; }
 
-       
+
         public decimal ChequesFC { get; set; }
 
-        
+
         public decimal TarjetasFC { get; set; }
 
-        
+
         public decimal OtrosMediosFC { get; set; }
 
-        
+
         public decimal TotalVendidoFC { get; set; }
 
-       
+
         public decimal TotalRegistradoFC { get; set; }
 
-        
+
         public decimal TotalAperturaFC { get; set; }
 
         public bool Activo { get; set; }
@@ -67,9 +67,48 @@ namespace NOVAAPP.Models
 
         public decimal TotalizadoMonedas { get; set; }
 
+
         public decimal TransferenciasColones { get; set; }
 
 
         public decimal TransferenciasDolares { get; set; }
+
+
+        public decimal EfectivoColonesC { get; set; }
+
+
+        public decimal ChequesColonesC { get; set; }
+
+
+        public decimal TarjetasColonesC { get; set; }
+
+        public decimal OtrosMediosColonesC { get; set; }
+
+
+
+
+
+
+        public decimal EfectivoFCC { get; set; }
+
+
+        public decimal ChequesFCC { get; set; }
+
+
+        public decimal TarjetasFCC { get; set; }
+
+
+        public decimal OtrosMediosFCC { get; set; }
+
+
+
+
+
+
+
+        public decimal TransferenciasColonesC { get; set; }
+
+
+        public decimal TransferenciasDolaresC { get; set; }
     }
 }
